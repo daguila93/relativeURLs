93b980039ac0e4e439652f05064698b0059fbcaf
D'Aguila <34548216+daguila93@users.noreply.github.com>
2022-02-23 11:53:28 -0300

    Update relativeURLs.js

--- relativeURLs.js
+++ relativeURLs.js
@@ -17,3 +17,21 @@ function turnURLRelative(node) {
         node.srcset = node.srcset.replaceAll(urlReplaced, '')
     }
 }
+<html>
+	<head> 
+		<meta property="creator.productor"
+			  content="http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/NUMERO">
+	</head>
+
+
+	<body> 
+		<div id="barra-brasil" style="background:#7F7F7F; height: 20px; padding:0 0 0 10px;display:block;">
+			<ul id="menu-barra-temp" style="list-style:none;">
+				<li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED">
+					<a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a>
+				</li>
+			</ul>
+		</div>
+		<script defer="defer" src="//barra.brasil.gov.br/barra_2.0.js" type="text/javascript"></script>
+	</body>
+</html>
