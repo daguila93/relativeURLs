0654ab45433a20c95ba64f59119a97ac35b5790d
D'Aguila <34548216+daguila93@users.noreply.github.com>
2022-10-04 17:28:47 -0300

    Update relativeURLs.js

--- relativeURLs.js
+++ relativeURLs.js
@@ -4,29 +4,18 @@ const attributes = ['href', 'src', 'srcset'];
 tags.forEach(tag => document.querySelectorAll(tag).forEach(atributte => turnURLRelative(atributte)));
 
 function turnURLRelative(node) {
-  const regex = new RegExp('^https?://' + window.location.hostname)
+const regex = new RegExp('^https?://' + window.location.hostname)
 
-  attributes.forEach(attr => {
-    if (node.hasAttribute(attr) && node[attr].match(regex)) {
-      let url = new URL(node[attr]);
-      node[attr] = node[attr].replaceAll(url.origin, '');
-    }
-  })
+attributes.forEach(attr => {
+if (node.hasAttribute(attr) && node[attr].match(regex)) {
+let url = new URL(node[attr]);
+node[attr] = node[attr].replaceAll(url.origin, '');
+}
+})
 }
 
-const site = window.location.host;
-// const siteRegex = /\./gm;
-// const substSite = '\\.';
-// const resultSite = site.replace(siteRegex, substSite);
+const regex = /data-et-multi-view.{0,}">/m;
+const subst = `>`;
+let element = document.getElementsByClassName('et_pb_image_wrap et_pb_only_image_mode_wrap')
 
-const regex = `/https?:\\\/\\\/${site}/gm`;
-const subst = '';
-
-let classNames = ['et_pb_image_wrap', 'et_pb_image_wrap et_pb_only_image_mode_wrap'];
-
-classNames.forEach(
-    no => Array.from(document.getElementsByClassName(no))
-        .forEach(
-            node => node.innerHTML = node.innerHTML.replace(regex, subst)
-        )
-)
+element.forEach(node => node.innerHTML = node.innerHTML.replace(regex, subst))
