fce98628573e2c327d347d2736b325134f1d5119
D'Aguila <34548216+daguila93@users.noreply.github.com>
2022-10-04 10:14:40 -0300

    Update relativeURLs.js

--- relativeURLs.js
+++ relativeURLs.js
@@ -17,7 +17,7 @@ function turnURLRelative(node) {
 const regex = /data-et-multi-view.{0,}">/m;
 const subst = `>`;
 
-let classNames = [ 'et_pb_image_wrap et_pb_only_image_mode_wrap']; // 'et_pb_image_wrap',
+let classNames = ['et_pb_image_wrap', 'et_pb_image_wrap et_pb_only_image_mode_wrap'];
 let element = document.getElementsByClassName('et_pb_image_wrap et_pb_only_image_mode_wrap')
 
 classNames.forEach(no => document.getElementsByClassName(no).forEach(node => node.innerHTML = node.innerHTML.replace(regex, subst)))
