b61748280409b8d2ee9af1bcb10bf43772d84f11
D'Aguila <34548216+daguila93@users.noreply.github.com>
2022-10-05 08:57:39 -0300

    Update relativeURLs.js

--- relativeURLs.js
+++ relativeURLs.js
@@ -1,27 +1,33 @@
-const tags = ['a', 'img', 'source', 'video', 'link', 'script'];
+const tags = ['a', 'img', 'source', 'video', 'link', ''];
 const attributes = ['href', 'src', 'srcset'];
 
 tags.forEach(tag => document.querySelectorAll(tag).forEach(atributte => turnURLRelative(atributte)));
 
 function turnURLRelative(node) {
-const regex = new RegExp('^https?://' + window.location.hostname)
+    const regex = new RegExp('^https?://' + window.location.hostname)
 
-attributes.forEach(attr => {
-if (node.hasAttribute(attr) && node[attr].match(regex)) {
-let url = new URL(node[attr]);
-node[attr] = node[attr].replaceAll(url.origin, '');
+    attributes.forEach(attr => {
+        if (node.hasAttribute(attr) && node[attr].match(regex)) {
+            let url = new URL(node[attr]);
+            node[attr] = node[attr].replaceAll(url.origin, '');
+        }
+    })
 }
-})
-}
 
-    const regex = /data-et-multi-view.{0,}">/m;
-    const subst = `>`;
+const regex = /http:\\\/\\\/caferh\.uff\.br/gm;
+const subst = '';
 
-    let classNames = ['et_pb_image_wrap', 'et_pb_image_wrap et_pb_only_image_mode_wrap'];
+let classNames = ['et_pb_image_wrap'];
 
-    classNames.forEach(
-        no => Array.from(document.getElementsByClassName(no))
-            .forEach(
-                node => node.innerHTML = node.innerHTML.replace(regex, subst)
-            )
-    )
+classNames.forEach(
+    no => Array.from(document.getElementsByClassName(no))
+        .forEach(
+            node => node.innerHTML = node.innerHTML.replace(regex, subst)
+        )
+)
+
+const regex = /data-et-multi-view.{0,}">/m;
+const subst = `>`;
+let element = document.getElementsByClassName('et_pb_image_wrap et_pb_only_image_mode_wrap')
+
+element.forEach(node => node.innerHTML = node.innerHTML.replace(regex, subst))
