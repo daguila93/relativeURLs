9e616e5a2817256ed20bc6eedcdd242809674e84
D'Aguila <34548216+daguila93@users.noreply.github.com>
2022-10-06 09:50:42 -0300

    Update relativeURLs.js

--- relativeURLs.js
+++ relativeURLs.js
@@ -14,10 +14,28 @@ function turnURLRelative(node) {
     })
 }
 
-const regex = /https?:\\\/\\\/caferh\.uff\.br/gm;
+const url = window.location.hostname;
+
+const regexURL = /\./gm;
+
+// Alternative syntax using RegExp constructor
+// const regex = new RegExp('\\.', 'gm')
+
+const substURL = `\\.`;
+
+// The substituted value will be contained in the result variable
+const result = url.replace(regex, subst);
+
+console.log('Substitution result: ', result);
+
+// const re = new RegExp("\\b" + test + "\\b");
+
+
+
+const regex = new RegExp("/https?:\\\/\\\/" + result + "/gm;");
 const subst = '';
 
-let classNames = ['et_pb_image_wrap'];
+let classNames = ['et_pb_image_wrap', 'et_pb_image_wrap et_pb_only_image_mode_wrap'];
 
 classNames.forEach(
     no => Array.from(document.getElementsByClassName(no))
@@ -25,6 +43,10 @@ classNames.forEach(
             node => node.innerHTML = node.innerHTML.replace(regex, subst)
         )
 )
+
+
+
+
 
 const regex2 = /https?:\\\/\\\/turismo\.uff\.br/gm;
 const subst2 = '';
