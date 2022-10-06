8ee2e02e1dce8f217c7cbc9a86cc5d57f79ff658
D'Aguila <34548216+daguila93@users.noreply.github.com>
2022-10-06 10:34:34 -0300

    Update relativeURLs.js

--- relativeURLs.js
+++ relativeURLs.js
@@ -14,34 +14,26 @@ function turnURLRelative(node) {
     })
 }
 
-
-
-const url = window.location.hostname;
-
-const regexURL = new RegExp("\\.", "gm");
-
-// Alternative syntax using RegExp constructor
-// const regex = new RegExp('\\.', 'gm')
-
-const substURL = `\\.`;
-
-// The substituted value will be contained in the result variable
-const result = url.replace(regexURL, substURL);
-
-console.log('Substitution result: ', result);
-
-// const re = new RegExp("\\b" + test + "\\b");
-
-
-
-const regex = new RegExp("https?:\\\/\\\/" + result, "igm");
+const regex = /https?:\\\/\\\/caferh\.uff\.br/gm;
 const subst = '';
 
-let classNames = ['et_pb_image_wrap', 'et_pb_image_wrap et_pb_only_image_mode_wrap'];
+let classNames = ['et_pb_image_wrap'];
 
 classNames.forEach(
     no => Array.from(document.getElementsByClassName(no))
         .forEach(
             node => node.innerHTML = node.innerHTML.replace(regex, subst)
+        )
+)
+
+const regex2 = /https?:\\\/\\\/turismo\.uff\.br/gm;
+const subst2 = '';
+
+let classNames2 = ['et_pb_image_wrap et_pb_only_image_mode_wrap'];
+
+classNames2.forEach(
+    no => Array.from(document.getElementsByClassName(no))
+        .forEach(
+            node => node.innerHTML = node.innerHTML.replace(regex2, subst2)
         )
 )
